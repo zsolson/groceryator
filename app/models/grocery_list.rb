e99ae990308e181dc70511f8706c280959d9c1ff
Zack Olson <zsolson@gmail.com>
2014-02-27 19:58:32 -0600

    Switch list ordering to desc

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -1,5 +1,6 @@
 class GroceryList < ActiveRecord::Base
 	belongs_to :user
+	default_scope -> { order('created_at DESC') }
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :user_id, presence: true
 end
