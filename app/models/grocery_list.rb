a0d14a349fd0e75021065595eab0962b34ede9a8
Zack Olson <zsolson@gmail.com>
2014-02-27 19:59:57 -0600

    Grocery List pagination is 10

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -1,4 +1,5 @@
 class GroceryList < ActiveRecord::Base
+	self.per_page = 10
 	belongs_to :user
 	default_scope -> { order('created_at DESC') }
 	validates :name, presence: true, length: { maximum: 50 }
