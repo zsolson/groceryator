c8b150c78735991a3ba6ef9c995c4f0a0e673390
Zack Olson <zsolson@gmail.com>
2014-03-08 16:08:59 -0600

    Fix price.rb

--- app/models/price.rb
+++ app/models/price.rb
@@ -7,8 +7,10 @@ class Price < ActiveRecord::Base
 			self.count += 1
 			if (self.max < new_price)
 				self.max = new_price
+			end
 			if (self.min > new_price)
 				self.min = new_price
+			end
 			self.avg = self.total / self.count
 		end
 	end
